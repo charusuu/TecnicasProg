c81259624d5fe9b0931a14e7f845300e494a123d
Charles Júnio <165962786+charusuu@users.noreply.github.com>
2024-04-03 19:46:34 -0300

    Add files via upload

---
+++ triangulo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int altura, linha, coluna;
+
+    printf("Altura do triangulo:");
+    scanf("%d", &altura);
+
+    for (linha = 0; linha < altura; linha++) {
+        for (coluna = 0; coluna <= linha; coluna++) {
+            printf("#");
+        }
+        printf("\n");
+    }
+    return 0;
+}
